add96a9427c09ec5e1713e03c0f826f19e47c7ff
JustineCrenier <justine.crenier@gmail.com>
2018-10-11 12:27:11 +0200

    cinquieme serie

--- 3-Deuxième série/js/ex02.js
+++ 3-Deuxième série/js/ex02.js
@@ -2,6 +2,6 @@ const rayon =  prompt('Indiquer la taille de votre rayon');
 
 function resultat(){
 	
-	const surface = Math.PI * 2 * Math.pow( rayon ,2);
+	const surface = Math.PI * Math.pow( rayon ,2);
 	alert (surface);		
 }
--- 5-Quatrième série/js/ex03.js
+++ 5-Quatrième série/js/ex03.js
@@ -1,14 +1,13 @@
 const texte = document.getElementById('texte');
-const green = document.getElementsByClassName("color")[0];
-const red = document.getElementsByClassName("color")[1];
-const blue = document.getElementsByClassName("color")[2];
 
-green.addEventListener("click", function(){
-	texte.setAttribute('style', 'color: green;');		
-});
-red.addEventListener("click", function(){
-	texte.setAttribute('style', 'color: red;');		
-});
-blue.addEventListener("click", function(){
-	texte.setAttribute('style', 'color: blue;');		
-});
+let changeColor = (index, color) => {
+	const col = document.getElementsByClassName("color")[index];
+
+	col.addEventListener("click", function(){
+		texte.style.color = `${color}`;		
+	});
+}
+
+changeColor(0, 'green');
+changeColor(1, 'red');
+changeColor(2, 'blue');
