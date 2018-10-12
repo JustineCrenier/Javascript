bf1d1dfec18cfe80fc39b76c102800af5d5b0c0d
JustineCrenier <justine.crenier@gmail.com>
2018-10-12 22:57:24 +0200

    terminé pendu

--- 10-jeu du pendu/js/index.js
+++ 10-jeu du pendu/js/index.js
@@ -1,1 +1,48 @@
 const word = ['B','O','N','J','O','U','R',];
+let tab = [];
+let val;
+let flag = 0;
+let input=document.getElementById('lettre');
+
+const submit = document.getElementById('submit');
+
+//Fonction guessLetter
+let guessLetter = (val) => {
+
+	flag = 0;
+	for(let i=0; i<word.length; i++){
+		if(val == word[i]){
+			tab.splice(i, 0, val);
+			let span = document.getElementById(`lettre${i}`);
+			span.innerHTML = val;
+			   
+   			if (flag == 0) {
+   				alert('Félicitations, tu as trouvé une lettre !\nEntre une nouvelle lettre.');
+   			}	
+   
+			flag = 1;
+		}
+	}
+	if(flag == 0){
+		alert("Pas de chance ! rentrez une nouvelle lettre");
+	}
+	if(word.length == tab.length){
+		alert("Gagné !")
+	}
+}
+
+//Eventlistener
+submit.addEventListener('click', function(val){
+	
+	if(word.length != tab.length){
+		val = document.getElementById('lettre').value;
+		guessLetter(val);
+	}
+	// input.setAttribute('value', null);
+});
+
+
+
+
+
+
